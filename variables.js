b75221212cce3a657e130d0f2e0e7a72d2eda01d
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-03-13 21:44:10 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,5 +1,21 @@
 /* What is variable
+Variables in JavaScript are used to store data values.
+They act like containers that hold information which can be used later in program.
 
-A variable is simply the name of a storage location
+Java has three ways to declare variables:
+1.var
+2.let
+3.const
 */
 
+//Example 1: using var 
+var name = "Aryan";
+console.log(name);
+
+//Example 2: using let 
+let age = 20;
+console.log(age);
+
+//Example 3: using const
+const country = "India";
+console.log(country);
