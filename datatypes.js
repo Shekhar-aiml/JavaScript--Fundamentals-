1027558ceadc5dc2989aba83f8d6ede69fadc34d
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-03-14 19:39:16 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,20 @@
+/* Datatypes in JavaScipt 
+Data types define the type of value a varialbe can hold in JavaScript
+*/
+
+let name = "Aryan";    //String
+let age = 20;           //Number
+let isStudent = true    //Boolean
+let score = null        //Null
+let value;             //Undefined 
+
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(score);
+console.log(value);
+
+
+
 
