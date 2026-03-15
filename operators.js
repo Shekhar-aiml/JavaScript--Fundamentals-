7cff66928229a76bb272703ed00752b07c61b9d8
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-03-15 20:18:50 +0530

    Implement assignment operators in operators.js
    Add assignment operators demonstration in operators.js

--- operators.js
+++ operators.js
@@ -10,3 +10,16 @@ console.log("Multiplication:", a * b);
 console.log("Division:", a / b);
 console.log("Modulus:", a % b);
 console.log("Exponent:", a ** b);
+
+//Assignment Operators 
+
+let a = 10;
+
+a += 5;
+console.log(a);
+
+a -= 3;
+console.log(a);
+
+a *= 2;
+console.log(a);
